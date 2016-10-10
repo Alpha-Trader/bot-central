55f02c71e6d6676142c2804e346ba12ad74974a4
frangelo <frangelo@frangelo-PC.fritz.box>
2016-10-10 15:05:42 +0200

    Added windows depencies and added basic login

--- src/main/java/BotCentral.java
+++ src/main/java/BotCentral.java
@@ -1,3 +1,4 @@
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.mashape.unirest.http.HttpResponse;
@@ -16,23 +17,35 @@ public class BotCentral {
 	public static void main(String[] args) {
 		String token = "";
 		try {
-		  HttpResponse<JsonNode> response = Unirest.post("http://stable.alpha-trader.com/user/token/")
-		                                           .header("accept", "*/*")
-		                                           .queryString("username", "<Benutzername>")
-		                                           .field("password", "<Passwort>")
-		                                           .asJson();
-		  JSONObject body = response.getBody().getObject();
+			HttpResponse<JsonNode> response = Unirest.post("http://stable.alpha-trader.com/user/token/")
+					.header("accept", "*/*").queryString("username", "Steerforth").field("password", "lollies")
+					.asJson();
+			JSONObject body = response.getBody().getObject();
 
-		  if (body.optInt("code", -1) == 200) {
-		    token = body.getString("message");
-		  } else {
-		    System.err.println("Login failed.");
-		  }
+			if (body.optInt("code", -1) == 200) {
+				token = body.getString("message");
+			} else {
+				System.err.println("Login failed.");
+			}
 		} catch (UnirestException e) {
-		  System.err.println("Login error: " + e.getMessage());
+			System.err.println("Login error: " + e.getMessage());
 		}
-		
+
 		System.out.println(token);
+
+		try {
+			HttpResponse<JsonNode> response = Unirest.get("http://stable.alpha-trader.com/api/companies/")
+					.header("accept", "*/*").header("Authorization", "Bearer " + token)
+					.header("X-Authorization", "e1d149fb-0b2a-4cf5-9ef7-17749bf9d144").asJson();
+
+			JSONArray companyNodes = response.getBody().getArray();
+
+			for (int i = 0; i < companyNodes.length(); i++) {
+				System.out.println(companyNodes.getJSONObject(i).toString(2));
+			}
+		} catch (UnirestException e) {
+			System.err.println("Error fetching companies: " + e.getMessage());
+		}
 	}
 
 }
