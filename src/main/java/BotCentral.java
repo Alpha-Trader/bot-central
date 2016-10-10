edc8b01c1d3a151d15c7c282822cf8626aa90154
Christopher Guckes <christopher.guckes@torq-dev.de>
2016-10-10 18:41:48 +0200

    Initial maven setup

---
+++ src/main/java/BotCentral.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author christopher.guckes@torq-dev.de
+ *
+ */
+public class BotCentral extends Application {
+
+	/* (non-Javadoc)
+	 * @see javafx.application.Application#start(javafx.stage.Stage)
+	 */
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
