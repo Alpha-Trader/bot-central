8eda7b0ba126e70c877b5480ff55b216197ba7b8
frangelo <frangelo@frangelo-PC.fritz.box>
2016-10-10 14:31:43 +0200

    Added dependencies and changed source level to 1.8

--- src/main/java/BotCentral.java
+++ src/main/java/BotCentral.java
@@ -1,27 +1,38 @@
-import javafx.application.Application;
-import javafx.stage.Stage;
+import org.json.JSONObject;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 /**
  * @author christopher.guckes@torq-dev.de
  *
  */
-public class BotCentral extends Application {
-
-	/* (non-Javadoc)
-	 * @see javafx.application.Application#start(javafx.stage.Stage)
-	 */
-	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
-
-	}
-
+public class BotCentral {
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String token = "";
+		try {
+		  HttpResponse<JsonNode> response = Unirest.post("http://stable.alpha-trader.com/user/token/")
+		                                           .header("accept", "*/*")
+		                                           .queryString("username", "<Benutzername>")
+		                                           .field("password", "<Passwort>")
+		                                           .asJson();
+		  JSONObject body = response.getBody().getObject();
 
+		  if (body.optInt("code", -1) == 200) {
+		    token = body.getString("message");
+		  } else {
+		    System.err.println("Login failed.");
+		  }
+		} catch (UnirestException e) {
+		  System.err.println("Login error: " + e.getMessage());
+		}
+		
+		System.out.println(token);
 	}
 
 }
