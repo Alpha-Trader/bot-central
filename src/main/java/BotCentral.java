6b165cb951353a093f1312ff17e231aa11451b98
Leon Bergmann <leon.bergmann@sky-lab.de>
2016-10-11 00:02:04 +0200

    splitted up

--- src/main/java/BotCentral.java
+++ src/main/java/BotCentral.java
@@ -8,60 +8,72 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 
 /**
  * @author christopher.guckes@torq-dev.de
- *
  */
 public class BotCentral {
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		String token = "";
-		try {
-			HttpResponse<JsonNode> response = Unirest.post("http://stable.alpha-trader.com/user/token/")
-					.header("accept", "*/*").queryString("username", "Steerforth").field("password", "lollies")
-					.asJson();
-			JSONObject body = response.getBody().getObject();
 
-			if (body.optInt("code", -1) == 200) {
-				token = body.getString("message");
-			} else {
-				System.err.println("Login failed.");
-			}
-		} catch (UnirestException e) {
-			System.err.println("Login error: " + e.getMessage());
-		}
+    /**
+     * @var string
+     */
+    private String apiUrl = "http://stable.alpha-trader.com/api/";
 
-		System.out.println(token);
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        token = this.getToken();
+        this.getCompanies(token);
+        this.getBonds(token);
+    }
 
-		try {
-			HttpResponse<JsonNode> response = Unirest.get("http://stable.alpha-trader.com/api/companies/")
-					.header("accept", "*/*").header("Authorization", "Bearer " + token)
-					.header("X-Authorization", "e1d149fb-0b2a-4cf5-9ef7-17749bf9d144").asJson();
+    private String getToken() {
+        String token = "";
+        try {
+            HttpResponse<JsonNode> response = Unirest.post(this.apiUrl + "user/token/")
+                    .header("accept", "*/*").queryString("username", "Steerforth").field("password", "lollies")
+                    .asJson();
+            JSONObject body = response.getBody().getObject();
 
-			JSONArray companyNodes = response.getBody().getArray();
+            if (body.optInt("code", -1) == 200) {
+                token = body.getString("message");
+            } else {
+                System.err.println("Login failed.");
+            }
+        } catch (UnirestException e) {
+            System.err.println("Login error: " + e.getMessage());
+        }
 
-			for (int i = 0; i < companyNodes.length(); i++) {
-				System.out.println(companyNodes.getJSONObject(i).toString(2));
-			}
-		} catch (UnirestException e) {
-			System.err.println("Error fetching companies: " + e.getMessage());
-		}
-	
+        return token;
+    }
 
-	try {
-		HttpResponse<JsonNode> response = Unirest.get("http://stable.alpha-trader.com/api/bonds/")
-				.header("accept", "*/*").header("Authorization", "Bearer " + token)
-				.header("X-Authorization", "e1d149fb-0b2a-4cf5-9ef7-17749bf9d144").asJson();
-		
-		JSONArray companyBonds = response.getBody().getArray();
-		
-		for (int i = 0; i < companyBonds.length(); i++){
-			System.out.println(companyBonds.getJSONObject(i).toString(2));
-		}
-	} catch (UnirestException e) {System.err.println("Error fetching bonds : " + e.getMessage());
-		
+    private void getCompanies(String token) {
+        try {
+            HttpResponse<JsonNode> response = Unirest.get(this.apiUrl + "companies/")
+                    .header("accept", "*/*").header("Authorization", "Bearer " + token)
+                    .header("X-Authorization", "e1d149fb-0b2a-4cf5-9ef7-17749bf9d144").asJson();
 
-	}
+            JSONArray companyNodes = response.getBody().getArray();
 
-}
+            for (int i = 0; i < companyNodes.length(); i++) {
+                System.out.println(companyNodes.getJSONObject(i).toString(2));
+            }
+        } catch (UnirestException e) {
+            System.err.println("Error fetching companies: " + e.getMessage());
+        }
+    }
+
+    private void getBonds(String token) {
+        try {
+            HttpResponse<JsonNode> response = Unirest.get(this.apiUrl + "bonds/")
+                    .header("accept", "*/*").header("Authorization", "Bearer " + token)
+                    .header("X-Authorization", "e1d149fb-0b2a-4cf5-9ef7-17749bf9d144").asJson();
+
+            JSONArray companyBonds = response.getBody().getArray();
+
+            for (int i = 0; i < companyBonds.length(); i++) {
+                System.out.println(companyBonds.getJSONObject(i).toString(2));
+            }
+        } catch (UnirestException e) {
+            System.err.println("Error fetching bonds : " + e.getMessage());
+        }
+    }
 }
