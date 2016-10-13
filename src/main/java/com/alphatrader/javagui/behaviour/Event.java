2110037009405637566472a036069325fc2404b8
Christopher Guckes <christopher.guckes@torq-dev.de>
2016-10-13 18:47:13 +0200

    Commented event

--- src/main/java/com/alphatrader/javagui/behaviour/Event.java
+++ src/main/java/com/alphatrader/javagui/behaviour/Event.java
@@ -3,6 +3,9 @@ package com.alphatrader.javagui.behaviour;
 /**
  * Different states and events that bots can react to.
  */
-public enum Event {
+enum Event {
+    /**
+     * This event is provided if the company has less than 100 monies.
+     */
     OUT_OF_MONEY
 }
