b9424b2aeacac8d615016857907eef8d398f280b
Christopher Guckes <christopher.guckes@torq-dev.de>
2016-10-13 19:10:02 +0200

    Added testcase for user

---
+++ src/test/java/com/alphatrader/javagui/data/UserTest.java
@@ -0,0 +1,37 @@
+package com.alphatrader.javagui.data;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Testcase for the {@link User} class.
+ *
+ * @author Christopher Guckes (christopher.guckes@torq-dev.de)
+ * @version 1.0
+ */
+public class UserTest {
+    private User toTest;
+
+    @Before
+    public void setUp() throws Exception {
+        toTest = new User("test", "pw");
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+        assertEquals("test", toTest.getName());
+    }
+
+    @Test
+    public void testGetPassword() throws Exception {
+        assertEquals("pw", toTest.getPassword());
+    }
+
+    @Test
+    public void testGetToken() throws Exception {
+        assertNull(toTest.getToken());
+    }
+
+}
